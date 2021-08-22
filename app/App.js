e46f77746b2c9ea01e9aef19f8a576ea824d2fd8
Alisson <alissoncruz@gmail.com>
2021-08-22 00:36:30 -0500

    se installa firebase y el boilerplate del proyecto

---
+++ app/App.js
@@ -0,0 +1,8 @@
+export function App() {
+  const login = (document.getElementById("root").innerHTML = `<h1> Hola Mundo </h1>
+  <label for="email">Correo</label>
+        <input id="email" placeholder="Correo" type="text">
+        <label for="Contraseña">Correo</label>
+        <input id="password" placeholder="Contraseña" type="text">
+        <button id="login" type="submit">Registrarse</button>`);
+}
